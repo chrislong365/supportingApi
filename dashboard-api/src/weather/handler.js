09d4ff9430151d556c456dc1cc77991f6fe51425
chrislong365 <christopher@chrislong365.com>
2024-02-18 21:03:13 -0800

    initial push with serverless setup

---
+++ dashboard-api/src/weather/handler.js
@@ -0,0 +1,21 @@
+
+
+exports.handler = async(event) => {
+
+	var response = {
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+			"Access-Control-Allow-Methods": "GET"
+		},
+		statusCode: 500,
+		body: "something went wrong",
+	};
+	
+	response = {
+		statusCode: 200,
+		body: "successful"
+	}
+
+	return (response);
+};
